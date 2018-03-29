9a1fe54fea43036e9a07e5473d9fc0acf67eedad
Alan B Smith <a.bax.smith@gmail.com>
2018-03-29 00:36:50 -0600

    update to use context api

--- src/blocks/ButtonGroup/Button.js
+++ src/blocks/ButtonGroup/Button.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import { rem } from 'polished';
 
 const Button = styled.button`
-  background: transparent;
+  background: ${props => (props.active ? props.theme.colors[props.bgColor] : 'transparent')};
   border: 1px solid ${props => props.theme.colors.text};
-  color: ${props => props.theme.colors.text};
+  color: ${props => (props.active ? props.theme.colors.chrome000 : props.theme.colors.text)};
   font-size: ${props => rem(props.theme.dimensions.baseFontSize)};
   line-height: ${props => rem(props.theme.dimensions.baseFontSize * 2)};
   outline: none;
   padding: 0 ${props => rem(props.theme.dimensions.baseGrid)};
+  text-transform: uppercase;
   transition: all 200ms ease;
 
   &:hover {
@@ -19,11 +20,14 @@ const Button = styled.button`
 `;
 
 Button.propTypes = {
+  active: PropTypes.bool,
   bgColor: PropTypes.string,
+  name: PropTypes.oneOf(['off', 'slow', 'med', 'fast']).isRequired
 };
 
 Button.defaultProps = {
-  bgColor: 'blue',
+  active: false,
+  bgColor: 'blue'
 };
 
 export default Button;
--- src/components/CogSpinner/index.js
+++ src/components/CogSpinner/index.js
@@ -1,37 +1,86 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import { ButtonGroup, ContentWrapper } from '../../blocks';
+import { ButtonGroup as ButtonGroupBlock, ContentWrapper } from '../../blocks';
 import { CogIcon } from '../../elements';
 
+const COG_SPINNER_CONTEXT = '__COG_SPINNER_CONTEXT__';
+
+// ============================================================================= //
+
+function Icon(props, context) {
+  const { speed } = context[COG_SPINNER_CONTEXT];
+
+  return <CogIcon {...props} speed={speed} />;
+}
+
+Icon.contextTypes = {
+  [COG_SPINNER_CONTEXT]: PropTypes.object.isRequired
+};
+
+// ============================================================================= //
+
+function ButtonGroup({ children, ...props }, context) {
+  const { handleClick, speed } = context[COG_SPINNER_CONTEXT];
+
+  const mappedChildren = React.Children.map(children, (child, i) => {
+    const active = speed === child.props.name;
+    return React.cloneElement(child, {
+      active,
+      onClick: handleClick
+    });
+  });
+
+  return <ButtonGroupBlock {...props}>{mappedChildren}</ButtonGroupBlock>;
+}
+
+ButtonGroup.contextTypes = {
+  [COG_SPINNER_CONTEXT]: PropTypes.object.isRequired
+};
+
+// ============================================================================= //
+
 class CogSpinner extends Component {
+  static Icon = Icon;
+  static ButtonGroup = ButtonGroup;
+  static Button = ButtonGroupBlock.Button;
+
+  static childContextTypes = {
+    [COG_SPINNER_CONTEXT]: PropTypes.object.isRequired
+  };
+
+  static defaultProps = {
+    onChange: () => {},
+    speed: 'off'
+  };
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    onChange: PropTypes.func,
+    speed: PropTypes.string
+  };
+
   state = {
     speed: this.props.speed
   };
 
+  getChildContext() {
+    return {
+      [COG_SPINNER_CONTEXT]: {
+        handleClick: this.handleClick,
+        speed: this.state.speed
+      }
+    };
+  }
+
   handleClick = e => {
+    this.props.onChange(e.target.name);
     this.setState({ speed: e.target.name });
   };
 
   render() {
-    return (
-      <ContentWrapper>
-        <CogIcon speed={this.state.speed} />
-        <ButtonGroup>
-          <ButtonGroup.Button name="off" onClick={this.handleClick} bgColor="deepPurple">
-            Off
-          </ButtonGroup.Button>
-          <ButtonGroup.Button name="slow" onClick={this.handleClick} bgColor="blue">
-            Slow
-          </ButtonGroup.Button>
-          <ButtonGroup.Button name="med" onClick={this.handleClick} bgColor="orange">
-            Med
-          </ButtonGroup.Button>
-          <ButtonGroup.Button name="fast" onClick={this.handleClick} bgColor="red">
-            Fast!
-          </ButtonGroup.Button>
-        </ButtonGroup>
-      </ContentWrapper>
-    );
+    const { children, ...rest } = this.props;
+    return <ContentWrapper {...rest}>{children}</ContentWrapper>;
   }
 }
 

1690e94f419884a659ae09c235f3223bb290b5c4
Alan B Smith <a.bax.smith@gmail.com>
2018-03-29 00:36:50 -0600

    update to use context api

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { ContentWrapper, Header } from '../../blocks';
+import { ContentWrapper, Header, Row } from '../../blocks';
 
 import CogSpinner from '../CogSpinner';
 
@@ -8,10 +8,54 @@ function App() {
   return (
     <ContentWrapper>
       <Header>
-        <Header.Title>Cog Spinner</Header.Title>
+        <Header.Title>Cog Spinners</Header.Title>
         <Header.Text>Click a button to animate the cog!</Header.Text>
       </Header>
-      <CogSpinner />
+      <CogSpinner>
+        <CogSpinner.Icon />
+        <CogSpinner.ButtonGroup>
+          <CogSpinner.Button name="off">Off</CogSpinner.Button>
+          <CogSpinner.Button name="slow">slow</CogSpinner.Button>
+        </CogSpinner.ButtonGroup>
+      </CogSpinner>
+      <CogSpinner>
+        <Row>
+          <CogSpinner.Icon />
+          <CogSpinner.Icon />
+        </Row>
+        <CogSpinner.ButtonGroup>
+          <CogSpinner.Button name="off" bgColor="red">
+            Off
+          </CogSpinner.Button>
+          <CogSpinner.Button name="med" bgColor="orange">
+            med
+          </CogSpinner.Button>
+          <CogSpinner.Button name="fast" bgColor="green">
+            fast
+          </CogSpinner.Button>
+        </CogSpinner.ButtonGroup>
+      </CogSpinner>
+      <CogSpinner>
+        <Row>
+          <CogSpinner.Icon />
+          <CogSpinner.Icon />
+          <CogSpinner.Icon />
+        </Row>
+        <CogSpinner.ButtonGroup>
+          <CogSpinner.Button name="off" bgColor="deepPurple">
+            Off
+          </CogSpinner.Button>
+          <CogSpinner.Button name="slow" bgColor="blue">
+            slow
+          </CogSpinner.Button>
+          <CogSpinner.Button name="med" bgColor="orange">
+            med
+          </CogSpinner.Button>
+          <CogSpinner.Button name="fast" bgColor="red">
+            fast
+          </CogSpinner.Button>
+        </CogSpinner.ButtonGroup>
+      </CogSpinner>
     </ContentWrapper>
   );
 }
--- src/elements/CogIcon/index.js
+++ src/elements/CogIcon/index.js
@@ -18,11 +18,14 @@ const SPEED_MAP = {
 };
 
 const CogIcon = styled.span.attrs({
-  className: 'ix-cog'
+  className: 'ix-cog',
+  'aria-label': 'spinning cog icon'
 })`
   animation: ${props => SPEED_MAP[props.speed]}ms ${spin} linear infinite;
   display: inline-block;
   font-size: 144px;
+  margin: 8px;
+
   &:before {
     color: ${props => props.theme.colors.chrome600};
   }
